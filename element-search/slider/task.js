bf176bba300950625bf19bc7acb90403f8f74bd4
Sergey Kotsulevskiy <seriy_552@bk.ru>
2022-01-23 19:38:11 +0300

    Task3 on findIndex, bags in index

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,7 +3,7 @@ let arrowRight = arrow[1];
 let arrowLeft = arrow[0];
 let slides = Array.from(document.querySelectorAll(".slider__item"));
 
-let slideInd = 0;
+/*let slideInd = 0;
 
 arrowRight.onclick = function () {
 	slideInd++;
@@ -26,5 +26,35 @@ arrowLeft.onclick = function () {
 	}
 	slides[slideInd].classList.add("slider__item_active");
 };
+*/
+function seachIndex (item) {
+	 item.className.includes("slider__item slider__item_active");
+}
 
+arrowRight.onclick = function () {
+	let positionStart = slides.findIndex(seachIndex);
+	if (positionStart === -1) {
+		return
+	} else {
+		slides[positionStart].classList.remove("slider__item_active");
+	}
+	if (positionStart = slides.length - 1) {
+		positionStart = 0;
+	} else {
+		slides[(positionStart + 1)].classList.add("slider__item_active")
+	}
+}
 
+arrowLeft.onclick = function () {
+	let positionEnd = slides.findIndex(seachIndex);
+	if (positionEnd === -1) {
+		return
+	} else {
+		slides[positionEnd].classList.remove("slider__item_active");
+	}
+	if (positionEnd = 0) {
+		positionEnd = slides.length -1;
+	} else {
+		slides[(positionEnd - 1)].classList.add("slider__item_active")
+	}
+}
