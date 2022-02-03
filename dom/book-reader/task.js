70feaa1c91e0d6620eb43f04d577e15e1cdf58ab
Sergey Kotsulevskiy <seriy_552@bk.ru>
2022-02-03 18:22:46 +0300

    Task DOM-book fixed

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,20 +1,23 @@
-//let fonts = Array.from(document.querySelectorAll('.font-size')); 
+let fonts = Array.from(document.querySelectorAll('.font-size')); 
 
-let fonts = Array.from(document.querySelectorAll('div.book__control a'));
+//let fonts = Array.from(document.querySelectorAll('div.book__control a'));
+let content = document.querySelector('.book__content');
 
 for (let i = 0; i < fonts.length; i++) {
     fonts[i].addEventListener('click', function (evt) {
-        evt.preventDefault();
+       evt.preventDefault();
 
         let indexActiveElement = fonts.findIndex((item) => item.classList.contains('font-size_active'));
         fonts[indexActiveElement].classList.remove('font-size_active');
-        document.getElementById('book').className === 'book';
-
+        content.className = 'book__content';
         fonts[i].classList.add('font-size_active');
-        if (fonts[i].classList.contains('font-size_small font-size_active')) {
-            document.getElementById('book').classList.add('book_fs-small');
-        } else if (fonts[i].classList.contains('font-size_big font-size_active')) {
-            document.getElementById('book').classList.add('book_fs-big');
+        
+        
+        if (fonts[i].classList.contains('font-size_small') && fonts[i].classList.contains('font-size_active')) {
+            content.classList.add('book_fs-small');
+        } else if (fonts[i].classList.contains('font-size_big') && fonts[i].classList.contains('font-size_active')) {
+            content.classList.add('book_fs-big');
         };
     });
 };
+
