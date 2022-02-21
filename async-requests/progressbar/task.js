4a98dad8e7a73ebbeebda522067409aa5a1a922c
Sergey Kotsulevskiy <seriy_552@bk.ru>
2022-02-21 23:05:09 +0300

    Asynh task 3 fix2

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,18 +1,23 @@
 let progress = document.getElementById('progress');
 let form = document.getElementById('form');
-let xhr = new XMLHttpRequest();
 
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    let dataForm = new FormData();
-    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-  
+   let xhr = new XMLHttpRequest();
+   let dataForm = new FormData();
+   xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
     xhr.upload.addEventListener('progress', (evt) => {
-        progress.value = evt.loaded / evt.total * 100;
+        progress.value = evt.loaded / evt.total *1000;
     });
-   
     xhr.upload.addEventListener('loadend', () => {
         progress.value = 1;
     });
-      xhr.send(dataForm);
+     
+    xhr.upload.addEventListener('load', () => {
+        alert('Файл загружен');
+    });
+
+    xhr.send(dataForm);
 });
+
+
