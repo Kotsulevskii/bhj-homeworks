eaee4fb8d3789e25e0b1cc368fef0ba6a9ea4bf0
Sergey Kotsulevskiy <seriy_552@bk.ru>
2022-02-23 21:41:17 +0300

    Asynh FORMgit status!

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,10 +4,10 @@ let form = document.getElementById('form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
    let xhr = new XMLHttpRequest();
-   let dataForm = new FormData();
+   let dataForm = new FormData(form);
    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
     xhr.upload.addEventListener('progress', (evt) => {
-        progress.value = evt.loaded / evt.total *1000;
+        progress.value = evt.loaded / evt.total;
     });
     xhr.upload.addEventListener('loadend', () => {
         progress.value = 1;
