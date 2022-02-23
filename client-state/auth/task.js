8bfe19124c230159d223513ea64603b6b1579940
Sergey Kotsulevskiy <seriy_552@bk.ru>
2022-02-23 23:38:01 +0300

    Client-state Fix task3

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -5,7 +5,7 @@ let userId = document.getElementById('user_id');
 
 let xhr = new XMLHttpRequest(); 
 
-form.onsubmit = function(e) {
+form.onsubmit = function (e) {
     e.preventDefault();
     let formData = new FormData(form);
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
@@ -13,18 +13,24 @@ form.onsubmit = function(e) {
     xhr.send(formData);
 }
 
-xhr.onload = function() {
-    if(xhr.status != 200) {
+xhr.onload = function () {
+    if (xhr.status != 200) {
         alert(`Ошибка ${xhr.status} : ${xhr.statusText}`);
+    } else if (!xhr.response.success) {
+        alert('Неверный логин/пароль');
     } else {
-        if(!xhr.response.success) {
-            alert('Неверный логин/пароль');
-        } else {
-            let idA = xhr.response.user_id;
-            userId.textContent = idA;
-            signin.classList.remove('signin_active');
-            welcome.classList.add('welcome_active');
-            localStorage.usaid = `${idA}`;
-        }
-    }
-}
+        let idA = xhr.response.user_id;
+        userId.textContent = idA;
+        signin.classList.remove('signin_active');
+        welcome.classList.add('welcome_active');
+        localStorage.usaid = `${idA}`;
+    };
+};
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.usaid) {
+        signin.classList.remove('signin_active');
+        welcome.classList.add('welcome_active');
+        userId.textContent = localStorage.usaid;
+    };
+});
+
